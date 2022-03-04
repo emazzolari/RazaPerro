68ba288e98d8c1b23fc132b53fe8180148339af5
emazzolari <100243239+emazzolari@users.noreply.github.com>
2022-03-03 21:12:37 -0300

    segundo commit

--- src/RazaPerroServicio/RazaPerroServicio.java
+++ src/RazaPerroServicio/RazaPerroServicio.java
@@ -3,6 +3,8 @@ package RazaPerroServicio;
 import java.util.ArrayList;
 import java.util.Scanner;
 import RazaPerroEntidades.RazaPero;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class RazaPerroServicio {
 
@@ -14,27 +16,48 @@ public class RazaPerroServicio {
         this.listaRaza = new ArrayList<>(); // Creo el espacio en memoria del LISTA
         this.leer = new Scanner(System.in).useDelimiter("\n");// Cuando se invoca la clase, habilita el flujo de datos
     }
-    
+
     public void cargarRaza() {
         //RazaPero p1 = new RazaPero();
         String respuesta = "";
         String p1;
         do {
-            
+
             System.out.println("Ingrese una raza");
-            
-            p1=(leer.next());
-            
+
+            p1 = (leer.next());
+
             listaRaza.add(new RazaPero(p1));
-            
+
             System.out.println("Quiere ingresar otra raza ?");
             respuesta = leer.next();
-            
+
         } while (respuesta.equalsIgnoreCase("Si"));
-        
+
         for (RazaPero razaPero : listaRaza) {
             System.out.println(razaPero.getRaza());
         }
     }
-}
 
+    public void buscarElimar() {
+
+        System.out.println("Ingrese la raza para eliminar");
+        String elimraza = leer.next();
+        boolean aux = false;
+        Iterator<RazaPero> it = listaRaza.iterator();
+        while (it.hasNext()) {
+            if (it.next().getRaza().equals(elimraza)) { // Si se cumple la condicion de encontrar "Ese apellido elegido,lo elimina)
+                it.remove(); aux=true; System.out.println("La raza se elimino");
+            }else;           
+        }
+       if (aux == false){
+           System.out.println("La raza no se encuentra");
+       }
+    }
+
+public void ordenarRaza() {
+        Collections.sort(listaRaza,
+                (RazaPero objeto1, RazaPero objeto2) -> objeto1.getRaza().compareTo(objeto2.getRaza()));
+        System.out.println(listaRaza);
+    }
+}
--- src/coleccionesejercicio1/ColeccionesEjercicio1.java
+++ src/coleccionesejercicio1/ColeccionesEjercicio1.java
@@ -1,26 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package coleccionesejercicio1;
 
 import RazaPerroServicio.RazaPerroServicio;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author Usuario
- */
 public class ColeccionesEjercicio1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         RazaPerroServicio p1 = new RazaPerroServicio();
         p1.cargarRaza();
-
+        p1.buscarElimar();
+        p1.ordenarRaza();
     }
 }
